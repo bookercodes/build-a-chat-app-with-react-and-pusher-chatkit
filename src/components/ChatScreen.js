c8f8210c37c3aa0364f01b067d8c77dd8a3f19f8
Amillz70 <adammiller7@yahoo.com>
2018-12-20 13:37:51 -0500

    Import Chatkit
    Make routes point to authentication point
    ChatManager should be able to connect now

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
+import Chatkit from '@pusher/chatkit-client'
 
 class ChatScreen extends Component {
+constructor(props) {
+  super(props)
+  this.state = {
+    currentUser: {}
+  }
+}
+
+componentDidMount () {
+  const chatManager = new Chatkit.ChatManager({
+    instanceLocator: 'v1:us1:92f6e0ea-dddd-478f-bca6-17bbc528eb78',
+    userId: this.props.currentUsername,
+    tokenProvider: new Chatkit.TokenProvider({
+      url: 'http://localhost:3001/authenticate',
+    }),
+  })
+
+  chatManager
+  .connect()
+  .then(currentUser => {
+    this.setState({ currentUser })
+  })
+  .catch(error => console.error('error', error))
+}
+
   render() {
-    return (
-      <div>
-      <h1>Chat</h1>
-      </div>
-    )
+    const styles = {
+      container:{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column'
+      }
+    }
   }
 }
 
