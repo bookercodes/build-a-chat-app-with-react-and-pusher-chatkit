612c7dd5e10f1d7a90aa16c85f28f27f20715672
Amillz70 <adammiller7@yahoo.com>
2018-12-20 15:16:38 -0500

    Add basic styling components to ChatScreen.js
    PLACEHOLDER to be removed later

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -32,8 +32,38 @@ componentDidMount () {
         height: '100vh',
         display: 'flex',
         flexDirection: 'column'
-      }
+      },
+      chatContainer: {
+        display: 'flex',
+        flex: 1,
+      },
+      whosOnlineListContainer: {
+        width: '300px',
+        flex: 'none',
+        padding: 20,
+        backgroundColor: '#2c303b',
+        color: 'white',
+      },
+      chatListContainer: {
+        padding: 20,
+        width: '85%',
+        display: 'flex',
+        flexDirection: 'column',
+      },
     }
+
+    return (
+      <div style={styles.container}>
+        <div style={styles.chatContainer}>
+          <aside style={styles.whosOnlineListContainer}>
+              <h2>Whos online PLACEHOLDER</h2>
+          </aside>
+          <section style={styles.chatListContainer}>
+              <h2>Chat PLACEHOLDER</h2>
+          </section>
+        </div>
+      </div>
+    )
   }
 }
 
